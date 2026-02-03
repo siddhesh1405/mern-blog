43d358d7e779a4a54f5601ad2747210c07b1bf83
Siddhesh Kamble <siddheshkamble@Siddheshs-MacBook-Air.local>
2026-02-03 02:05:12 +0000

    Code Changes

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About />} />
       <Route path='/articles-list' element={<ArticlesList />} />
-      <Route path='/article' element={<Article />} />
+      <Route path='/article/:articleName' element={<Article />} />
       </Routes>
     </div>
     </Router>
